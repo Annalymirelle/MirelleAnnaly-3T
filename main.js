fc320f6b7fa2c49b67085ff9cd4899ea4cabc899
Annalymirelle <127766611+Annalymirelle@users.noreply.github.com>
2023-10-16 12:24:10 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
     function tocaSom(idElementoAudio){
-    document.querySelector(dElementoAudio).play();
+    document.querySelector(idElementoAudio).play();
 }
     const listaDeTeclas = document.querySelectorAll('.tecla');  
 
      let contador = 0;
      // Estrutura de Repetição -  Enquanto 
      while (contador < listaDeTeclas.length) {
-     listaDeTeclas[contador].onclick =  function (){  
-        tocaSom('som_tecla_splash');
+     listaDeTeclas[contador]
+        
     };
-    contador = contador + 1;  
+    contador = contador + 1; 
+    console.log(contador);
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+     const idAudio =  ` #som_${instrumento}`
+    console.log(idAudio);
+    tecla.onclick =  function (){  
+        tocaSom('som_tecla_splash');
      }
     
 
